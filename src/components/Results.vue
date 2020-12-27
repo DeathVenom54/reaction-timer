30644b9007017a09b5ddbdff4a7eb7c4490d2078
Deathvenom <65107979+Deathvenom54@users.noreply.github.com>
2020-12-27 11:31:34 +0530

    modified:   src/App.vue 	new file:   src/components/Block.vue 	deleted:    src/components/HelloWorld.vue 	new file:   src/components/Results.vue 	new file:   vue.config.js

---
+++ src/components/Results.vue
@@ -0,0 +1,23 @@
+<template>
+  <h1>{{ comment }}</h1>
+  <p>Reaction time: {{ time }} ms</p>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {}
+    },
+    computed: {
+      comment() {
+        if (this.time < 250) return 'Ninja Fingers!'
+        if (this.time < 400) return 'Rapid Reflexes'
+        else if (this.time < 500) return 'Can be improved'
+        else return 'Snail pace...'
+      },
+    },
+    props: ['time'],
+  }
+</script>
+
+<style></style>
